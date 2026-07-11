eab14fc8c78d37561c6abd031dae4617a96798c1
Harsh Prateek <Itsharshprateek@gmail.com>
2026-07-12 05:21:50 +0530

    Fix white screen: Import HelmetProvider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
